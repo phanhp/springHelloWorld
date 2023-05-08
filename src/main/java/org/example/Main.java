df7d02ba96091f86ace5956897a8af181c22d710
phan huu phuc <phanhuuphuc2520@gmail.com>
2023-05-08 20:09:23 +0700

    phan huu phuc

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.management.MBeanServer;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Account acc1 = (Account) context.getBean("account1");
+        System.out.println("account 1 id: "+ acc1.getId());
+    }
+}
